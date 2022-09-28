60c0d1aefba1c929d5bb8770db4a9bd171fbebe8
akshita garg <akshitagarg275@gmail.com>
2022-09-28 07:54:35 +0530

    day 3 : datatypes, typeconversion

---
+++ 01basics/03datatypes.py
@@ -0,0 +1,107 @@
+'''
+Datatypes -> primitive and derived
+'''
+
+# TODO: primitive datatypes
+
+# TODO: int
+
+# age = 15
+# print("age is : ",age)
+# print(type(age))
+
+# # TODO: float
+
+# price = 15.34
+# print("price is : ",price)
+# print(type(price))
+
+# TODO: complex (real num +/- imaginary num)
+# 5 + 3j
+
+# c = 5 + 3j
+# print("complex  is : ",c)
+# print(type(c))
+
+
+# TODO: boolean (True/False)
+# is_valid = True
+# print(is_valid)
+# print(type(is_valid))
+
+# is_admin = False
+# print(is_admin)
+# print(type(is_admin))
+
+# TODO: strings 
+# single and double quote strings are one line strings
+
+# s = 'we are learning ' \
+#     'python'
+# s = '"we are learning"'
+# print(s)
+# print(type(s))
+
+# m = "let's start playing"
+# print(m)
+# print(type(m))
+
+# msg = '''we
+# are
+# writing multline
+#         string
+# '''
+# print(msg)
+
+
+# TODO: None
+# temp = None
+# print(temp)
+# print(type(temp))
+
+# TODO: Derived datatypes
+
+# TODO: List []
+# It allows to store homogeneous and heterogeneous values
+# marks = [12, 13.5, 16, 17, 18]
+# print(marks)
+# print(type(marks))
+
+# TODO: tuple ()
+# marks = (12, 13.5, 16, 17, 18)
+# print(marks)
+# print(type(marks))
+
+# TODO: set { }
+# marks = {12, 13.5, 16, 17, 18, 16}
+# print(marks)
+# print(type(marks))
+
+# TODO: dictionary {key : value}
+# user = {
+#     "fname" : "John",
+#     "lname" :  "Doe",
+#     "age"   : 15 ,
+#     "course" : ["Python", "Computers"]
+# }
+
+# print(user)
+# print(type(user))
+
+# TODO: range
+# range (start, end, step)
+# start is included
+# end is not included
+
+# nums = list(range(1, 6))
+# print(nums)
+# print(type(nums))
+
+# even = tuple(range(0, 11, 2))
+# print(even)
+
+# num = list(range(6))
+# print(num)
+
+rev = tuple(range(10, 0, -1))
+print(rev)
---
+++ 01basics/04type_conversion.py
@@ -0,0 +1,25 @@
+# typecasting
+'''
+int()
+float()
+str()
+complex()
+bool()
+list()
+tuple()
+dict()
+set()
+'''
+
+# a = float(5)
+# a = str(5)
+# a = int('12')
+
+# will not work
+# a = int('ad')
+
+
+# print(a , type(a))
+
+name = list("John")
+print(name, type(name))
